655b62ac8db817882176092a05fd8cdb83339c90
Tilakg05 <sghorashainee24@gmail.com>
2022-08-04 14:42:45 -0700

    Add files via upload

---
+++ labex-05-TilakGhorashainee/EditableCrosswalkList/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  EditableCrosswalkList
+//
+//  Created by Paul Inventado on 3/31/22.
+//
+import SwiftUI
+struct ContentView: View {
+    @StateObject var manager = CrosswalkManager()
+    var body: some View {
+        // TODO: Model 4 - Replace VStack with TabView
+        TabView {
+            TabView {
+                        EditableCrosswalkList()
+                            .tabItem {
+                                Image(systemName: "car")
+                                Text("Crosswalks")
+                            }
+            CrossWalkInfo()
+                            .tabItem {
+                                Image(systemName: "info")
+                                Text("Crosswalk Info")
+                            }
+            AddCrossWalk()
+                .tabItem{
+                    Image(systemName:"pluse")
+                    Text("Add Crosswalk")
+                }
+                    
+                }.environmentObject(manager)
+            }
+    }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        }
+      }
+}
---
+++ labex-05-TilakGhorashainee/EditableCrosswalkList/CrosswalkViews.swift
@@ -0,0 +1,125 @@
+//
+//  CrosswalkViews.swift
+//  EditableCrosswalkList
+//
+//  Created by Paul Inventado on 3/31/22.
+//
+
+import SwiftUI
+
+struct EditableCrosswalkList: View {
+    @EnvironmentObject var manager: CrosswalkManager
+    var body: some View {
+        VStack {
+              
+                EditButton()
+                List {
+                ForEach(manager.crosswalks) {
+                crosswalk in
+                VStack (alignment: .leading) {
+                Text(crosswalk.name)
+                .font(.largeTitle)
+               Text(crosswalk.description)
+              .font(.caption)
+            }
+                }.onDelete {
+                offset in
+                manager.crosswalks.remove(atOffsets: offset)
+        }
+            .onMove {
+            offset, index in
+        manager.crosswalks.move(fromOffsets: offset,
+                toOffset: index)
+                 }
+                }
+             }
+           }
+        }
+struct CrossWalkInfo: View {
+     var body: some View {
+         NavigationView {
+             VStack {
+                 List {
+                     Section(header: Text("Crosswalk")) {
+                         NavigationLink(destination: Text("Name of the crosswalk")) {
+                             Text("Crosswalk name")
+                         }
+                         NavigationLink(destination: Text("Address of the crosswalk")) {
+                             Text("Crosswalk address")
+                         }
+                     }
+                     Section(header: Text("Volunteer")) {
+                         NavigationLink(destination: Text("Name of the volunteer")) {
+                             Text("Volunteer")
+                         }
+                         DisclosureGroup(content: {
+                             NavigationLink(destination: Text("Minors can only volunteer for 1 hour and accompanied by an adult.")) {
+                                 Text("Minors")
+                             }
+                             NavigationLink(destination: Text("Adults can volunteer for a maximum of 3 hours.")) {
+                                 Text("Adults")
+                             }
+                             NavigationLink(destination: Text("Seniors can volunteer for a maximum of 2 hours.")) {
+                                 Text("Seniors")
+                             }
+                         }) {
+                             Text("Maximum hours")
+                         }
+                     }
+                 }
+                 Spacer()
+             }
+         }
+     }
+ }
+
+ struct AddCrossWalk: View {
+     @SceneStorage("crosswalkName") var crosswalkName: String = ""
+     @SceneStorage("crosswalkAddress") var crosswalkAddress: String = ""
+     @EnvironmentObject var manager: CrosswalkManager
+     var body: some View {
+         NavigationView {
+             VStack {
+                 HStack {
+                 Text("Crosswalk Submission")
+                     .bold()
+                     .font(.largeTitle)
+                 }
+                 .padding(.bottom, 30)
+
+                 HStack {
+                     Text("Crosswalk Name")
+                         .bold()
+                     Spacer()
+                 }
+                 .padding(.bottom, 5)
+                 HStack {
+                     TextField("Crosswalk name", text: $crosswalkName)
+                         .modifier(TextEntry())
+                     Spacer()
+                 }
+                 .padding(.bottom, 20)
+                 HStack {
+                     Text("Crosswalk address")
+                         .bold()
+                     Spacer()
+                 }
+                 .padding(.bottom, 5)
+                 TextEditor(text: $crosswalkAddress)
+                     .modifier(TextEntry())
+                     .padding(.bottom, 30)
+                 Button(action: {
+                     manager.crosswalks.append(CrossWalk(name: crosswalkName, description: crosswalkAddress))
+                     crosswalkName = ""
+                     crosswalkAddress = ""
+                 }) {
+                     Text("Submit")
+                         .modifier(ButtonDesign())
+                 }
+                 Spacer()
+             }
+             .padding()
+         }
+     }
+ }
+
